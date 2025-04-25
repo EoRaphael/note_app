9c679e4c87d8a01ba87c594e614c7a81622f69e2
Raphael <raphael.oficial189@gmail.com>
2025-04-25 20:08:40 -0300

    formatação

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -2,8 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/services/firestore.dart';
 
-
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -13,41 +11,43 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // firestore
-  final FirestoreService firestoreService  = FirestoreService();
+  final FirestoreService firestoreService = FirestoreService();
 
   // text controller
   final TextEditingController textController = TextEditingController();
 
-
   //open a dialog box to add a note
   void openNoteBox({String? docID}) {
-    showDialog(context: context, builder: (context) => AlertDialog(
-      // text user input
-      content: TextField(
-        controller: textController,
-      ),
-      actions: [
-        // button to save
-        ElevatedButton(
-            onPressed: () {
-              // add a new note
-              if (docID == null) {
-                firestoreService.addNote(textController.text);
-              }
-              // update an existing note
-              else {
-                firestoreService.updateNote(docID, textController.text);
-              }
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            // text user input
+            content: TextField(controller: textController),
+            actions: [
+              // button to save
+              ElevatedButton(
+                onPressed: () {
+                  // add a new note
+                  if (docID == null) {
+                    firestoreService.addNote(textController.text);
+                  }
+                  // update an existing note
+                  else {
+                    firestoreService.updateNote(docID, textController.text);
+                  }
 
-              // clear the text controller
-              textController.clear();
+                  // clear the text controller
+                  textController.clear();
 
-              // close the box
-              Navigator.pop(context);
-            },
-            child: Text('Add'))
-      ],
-    ));
+                  // close the box
+                  Navigator.pop(context);
+                },
+                child: Text('Add'),
+              ),
+            ],
+          ),
+    );
   }
 
   @override
@@ -74,41 +74,41 @@ class _HomePageState extends State<HomePage> {
             return ListView.builder(
               itemCount: notesList.length,
               itemBuilder: (context, index) {
-              // get each individual doc
-              DocumentSnapshot document = notesList[index];
-              String docID = document.id;
+                // get each individual doc
+                DocumentSnapshot document = notesList[index];
+                String docID = document.id;
 
-              //get note from each doc
-              Map<String, dynamic> data =
-                  document.data() as  Map<String, dynamic>;
-              String noteText = data['note'];
-              // display as a list tile
-              return ListTile(
-                title: Text(noteText),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Update button
-                    IconButton(
+                //get note from each doc
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                String noteText = data['note'];
+                // display as a list tile
+                return ListTile(
+                  title: Text(noteText),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Update button
+                      IconButton(
                         onPressed: () => openNoteBox(docID: docID),
-                        icon: const Icon(Icons.settings)),
+                        icon: const Icon(Icons.settings),
+                      ),
 
-                    // Delete button
-                    IconButton(
+                      // Delete button
+                      IconButton(
                         onPressed: () => firestoreService.deleteNote(docID),
-                        icon: const Icon(Icons.delete))
-                  ]
-                ),
-              );
-            },
-          );
+                        icon: const Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
           }
-
           // if there is no data return nothing
           else {
             return const Text('No notes..');
           }
-
         },
       ),
     );
